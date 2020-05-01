58e37ddc7a358cb6de70efdb932247f88935bc99
quantumenigma18 <povartetiana@gmail.com>
2020-05-01 14:43:43 +0300

    Adding code outline and documentation for C1M1 Assessment

---
+++ README.md
@@ -0,0 +1,2 @@
+Author: Povar Tetiana
+Introduction to Embedded Systems Software and Development Environments.Week 1 Application Assignment.
--- stats.c
+++ stats.c
@@ -2,20 +2,20 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Assignment1
  *
- * <Add Extended Description Here>
+ * <Source file>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Tetiana Povar
+ * @date 01.05.2020
  *
  */
 
@@ -28,16 +28,49 @@
 #define SIZE (40)
 
 void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-
+    
+    unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+        114, 88,   45,  76, 123,  87,  25,  23,
+        200, 122, 150, 90,   92,  87, 177, 244,
+        201,   6,  12,  60,   8,   2,   5,  67,
+        7,  87, 250, 230,  99,   3, 100,  90};
+    
+    print_array(test,SIZE);
+    print_statistics(test,SIZE);
+    sort_array(test,SIZE);
+    print_array(test,SIZE);
+    
+}
+/* Add other Implementation File Code Here */
+void print_statistics(unsigned char *array, unsigned int length)
+{
+    
 }
 
-/* Add other Implementation File Code Here */
+void print_array(unsigned char *array, unsigned int length)
+{
+}
+
+void sort_array(unsigned char *array, unsigned int length)
+{
+    
+}
+
+int find_median(unsigned char *array, unsigned int length)
+{
+}
+
+int find_mean(unsigned char *array, unsigned int length)
+{
+   
+}
+int find_maximum(unsigned char *array, unsigned int length)
+{
+    }
+
+int find_minimum(unsigned char *array, unsigned int length)
+{
+   
+    
+}
+
